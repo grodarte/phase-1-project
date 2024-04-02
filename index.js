394802afa8289db72cbdcaf0de14c2fef81d3129
Gabrielle <ms.grodarte@gmail.com>
2024-04-02 15:47:39 -0700

    initial fetch

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("index.js is connected")
+    fetchData()
 })
+
+function fetchData() {
+    fetch("https://rickandmortyapi.com/api/character")
+    .then(res=>res.json())
+    .then(characterData=>{
+        console.log(characterData.results)
+    })
+}
