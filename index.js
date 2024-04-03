437b2b39bd43d2ca05f84722ec68fefc6fca61e1
Gabrielle <ms.grodarte@gmail.com>
2024-04-02 19:18:28 -0700

    working fetch requests

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("index.js is connected")
-    fetchData()
+    fetchCharacters()
+    fetchEpisodes()
 })
 
-function fetchData() {
+function fetchCharacters(){
     fetch("https://rickandmortyapi.com/api/character")
     .then(res=>res.json())
     .then(characterData=>{
+        console.log(characterData.info.pages + " pages of characters")
         console.log(characterData.results)
+    })
+}
+
+function fetchEpisodes(){
+    fetch("https://rickandmortyapi.com/api/episode")
+    .then(res=>res.json())
+    .then(episodeData=>{
+        console.log(episodeData.info.count + " number of episodes")
     })
 }
